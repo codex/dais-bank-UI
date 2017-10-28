097899736d5a561aff35d7b9ece8b6b17cd90675
svrakata <svrakata@gmail.com>
2017-10-29 00:14:38 +0300

    accounts/cards done/style fixes/tweaks

---
+++ js/data.js
@@ -0,0 +1,41 @@
+const data = {
+    accounts: {
+        transactions: [
+            {
+                date: '02.10.2017',
+                amount: 130,
+                currency: 'BGN',
+                type: 'Debit',
+                description: 'Latest WoW Expansion',
+            },
+            {
+                date: '07.10.2017',
+                amount: 40,
+                currency: 'BGN',
+                type: 'Debit',
+                description: 'Groceries for dinner',
+            },
+            {
+                date: '10.10.2017',
+                amount: 45,
+                currency: 'BGN',
+                type: 'Debit',
+                description: '3 tickets for the SW Last Jedi',
+            },
+            {
+                date: '11.10.2017',
+                amount: 96,
+                currency: 'BGN',
+                type: 'Debit',
+                description: 'Brakepads, can of DOT4',
+            },
+            {
+                date: '16.10.2017',
+                amount: 210,
+                currency: 'BGN',
+                type: 'Debit',
+                description: 'AudioTechnica Headset',
+            },
+        ],
+    },
+};
