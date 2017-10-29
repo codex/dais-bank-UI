03d8db8518c74f0bf478ec65cc1fc7937ddbbc51
svrakata <svrakata@gmail.com>
2017-10-29 22:54:51 +0200

    dev and dist versions

--- js/template.js
+++
@@ -1,9 +0,0 @@
-/* globals $ Handlebars */
-
-function fetchTemplate(template, viewData = null) {
-    return $.get(`templates/${template}.hbs`)
-        .then((hbs) => {
-            const html = Handlebars.compile(hbs);
-            return html(viewData);
-        });
-}
